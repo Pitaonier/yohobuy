26facf087d9d365143074a5f6c21e035e5ad80f7
houxuexue <772802412@qq.com>
2019-03-14 11:07:32 +0800

    one

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function shadow(){
+    
+}
+
+function $(str){   //id  class tagname
+    if(str.charAt(0) == "#"){
+        return document.getElementById(str.substring(1));
+    }else if(str.charAt(0) == "."){
+        return document.getElementsByClassName(str.substring(1));
+    }else{
+        return document.getElementsByTagName(str);
+    }
+}
